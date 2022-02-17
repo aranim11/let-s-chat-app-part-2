cb8733dee1855f9885026b5573fea69c295f32a4
aranim11 <93585588+aranim11@users.noreply.github.com>
2022-02-17 15:59:16 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBP6j_q9Nsuh5pi1uwEcPw854hBCrIUl1g",
+    authDomain: "chat-app-15bcc.firebaseapp.com",
+    databaseURL: "https://chat-app-15bcc-default-rtdb.firebaseio.com",
+    projectId: "chat-app-15bcc",
+    storageBucket: "chat-app-15bcc.appspot.com",
+    messagingSenderId: "416025600553",
+    appId: "1:416025600553:web:0e10af6b670852a679f238"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
